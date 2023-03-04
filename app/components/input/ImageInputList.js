8cde50f6a3c7d954c0f8fbf239f8bf09060d8def
Laurent Ouma <lawiomosh3@gmail.com>
2023-03-04 11:38:34 +0300

    Done with imageinput list and works perfectly fine, added auto scrolling to the ende whenever size of its content increases

--- app/components/input/ImageInputList.js
+++ app/components/input/ImageInputList.js
@@ -1,36 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useRef } from "react";
 import ImageInput from "./ImageInput";
 
 const ImageInputList = ({ localImagesList = [], onImagesListChange }) => {
+  const scrollViewRef = useRef();
   return (
-    <View style={styles.container}>
-      {localImagesList.map((localImage, index) => (
-        <View style={styles.item} key={index}>
-          <ImageInput
-            radiusScaleFactor={0.25}
-            localImage={localImage}
-            onImageChange={(image) => {
-              if (image) {
-                // add an image
-                onImagesListChange([...localImagesList, image]);
-              } else {
-                // delete this image
-                onImagesListChange(
-                  localImagesList.filter((img) => img.uri !== localImage.uri)
-                );
+    <View>
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        onContentSizeChange={() => {
+          scrollViewRef.current.scrollToEnd();
+        }}
+      >
+        <View style={styles.container}>
+          {localImagesList.map((localImage, index) => (
+            <View style={styles.item} key={index}>
+              <ImageInput
+                radiusScaleFactor={0.25}
+                localImage={localImage}
+                onImageChange={(image) => {
+                  if (image) {
+                    // add an image
+                    onImagesListChange([...localImagesList, image]);
+                  } else {
+                    // delete this image
+                    onImagesListChange(
+                      localImagesList.filter(
+                        (img) => img.uri !== localImage.uri
+                      )
+                    );
+                  }
+                }}
+              />
+            </View>
+          ))}
+          <View style={styles.item}>
+            <ImageInput
+              onImageChange={(image) =>
+                onImagesListChange([...localImagesList, image])
               }
-            }}
-          />
+            />
+          </View>
         </View>
-      ))}
-      <View style={styles.item}>
-        <ImageInput
-          onImageChange={(image) =>
-            onImagesListChange([...localImagesList, image])
-          }
-        />
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -40,8 +53,6 @@ export default ImageInputList;
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
   },
   item: {
     margin: 10,
